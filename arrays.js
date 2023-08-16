b68a723fde6b390a0fd8d4dc5e8302c7e4830f61
Menfi Garcia <menfigarcia31@gmail.com>
2023-08-16 11:18:59 -0600

    mystring

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let myString = "Javascript+es+super+cool";                          
+myArray = myString.split('+');
+console.log('mi Array :' , myArray);                                   
+
+let arrayLength = myArray;
+console.log('la longitud de mi array es :' , arrayLength.length);
+
+let lastitem = myArray;
+console.log('el ultimo item es :' , arrayLength.length - 1)
